b9fffcc1ff86485d72820f25cc6188c9d5eab9d0
Fresh Dev <billyhans90@gmail.com>
2024-08-24 14:34:55 +0100

    Get users endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import express, { NextFunction, Request, Response } from 'express';
+import userRouter from './routes/users';
 
 const app = express();
 
 const PORT = 3000;
 
-app.get('/api/users', (request: Request, response: Response, next: NextFunction) => {
-    response.send([]);
-})
+app.use('/api/users', userRouter);
 
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`);
