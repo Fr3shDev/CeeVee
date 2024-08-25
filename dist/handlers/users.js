a5bab21f7cb9a6cf852544b2014d38e56dd1c16c
Fresh Dev <billyhans90@gmail.com>
2024-08-25 14:56:24 +0100

    Get single user

---
+++ dist/handlers/users.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getUsers = getUsers;
+exports.getUser = getUser;
+function getUsers(request, response) {
+    response.send([]);
+}
+function getUser(request, response) {
+    response.send({});
+}
+//# sourceMappingURL=users.js.map
