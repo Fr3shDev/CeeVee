ac313fca5c1576eb039f513911ac64852f3739f7
Fresh Dev <billyhans90@gmail.com>
2024-08-27 09:12:40 +0100

    Type annotate query parameters

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,5 +1,6 @@
 import { CreateUserDto } from "dtos/CreateUser.dto";
 import { Request, Response } from "express";
+import { CreateUserQueryParams } from "types/query-params";
 
 export function getUsers(request: Request, response: Response) {
     response.send([]);
@@ -9,6 +10,6 @@ export function getUser(request: Request, response: Response) {
     response.send({});
 }
 
-export function createUser(request: Request<{}, {}, CreateUserDto>, response: Response) {
-    
+export function createUser(request: Request<{}, {}, CreateUserDto, CreateUserQueryParams>, response: Response) {
+    request.query.loginAfterCreate
 }
