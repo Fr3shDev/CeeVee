caca096b2ddf8c17ec212c60aa8a6ba7d97d0456
Fresh Dev <billyhans90@gmail.com>
2024-09-11 03:49:19 +0100

    Install types for morgan

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { createApp } from "./createApp";
 import passport from "passport";
 import dotenv from "dotenv";
+import helmet from "helmet";
 
 dotenv.config();
 
 const app = createApp();
+app.use(helmet());
 const port = process.env.PORT || 5000;
 
 
