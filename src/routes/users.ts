db1f80a26860cacaf10802c3c884edf27477df91
Fresh Dev <billyhans90@gmail.com>
2024-09-30 10:08:45 +0100

    Setup registration route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
 import { createUser, getUser, getUsers } from "../handlers/users";
+import { userSchema, validateUser } from "models/user";
+import mongoose from "mongoose";
 
 const router = Router();
+
+router.post('/', async (request, response) => {
+    
+})
 
 // /api/users
 router.get('/', getUsers);
