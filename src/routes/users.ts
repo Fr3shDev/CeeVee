6a57893afb242648b12b5334520af8bb141aea6b
Fresh Dev <billyhans90@gmail.com>
2024-10-22 09:08:56 +0100

    Use the try catch block for registration

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import { createUser, getUser, getUsers } from "../handlers/users";
 import { User, validateUser } from "models/user";
 import mongoose from "mongoose";
@@ -7,22 +7,26 @@ const router = Router();
 
 
 
-router.post('/', async (request: Request, response: Response) => {
-    const { error } = validateUser(request.body);
-    if (error) return response.status(400).send(error.details[0].message);
-
-    let user = await User.findOne({ email: request.body.email})
-    if (user) return response.status(400).send('User already registered');
-
-    user = new User({
-        username: request.body.username,
-        email: request.body.email,
-        password: request.body.password
-    });
-
-    await user.save();
-
-    response.send(user);
+router.post('/', async (request: Request, response: Response, next: NextFunction) => {
+    try{
+        const { error } = validateUser(request.body);
+        if (error) return response.status(400).send(error.details[0].message);
+    
+        let user = await User.findOne({ email: request.body.email})
+        if (user) return response.status(400).send('User already registered');
+    
+        user = new User({
+            username: request.body.username,
+            email: request.body.email,
+            password: request.body.password
+        });
+    
+        await user.save();
+    
+        response.send(user);
+    } catch (error) {
+        next(error);
+    }
 });
 
 // /api/users
