d09b5f23269811e3ef54209603d9c87869c1d064
Fresh Dev <billyhans90@gmail.com>
2024-12-12 11:06:58 +0100

    Move code to the handler file

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -4,23 +4,23 @@ import { User, validateUser } from "models/user";
 import { CreateUserQueryParams } from "types/query-params";
 import { UserResponseInterface } from "types/response";
 
-// export async function registerUser (request: Request, response: Response, next: NextFunction) {
-//     const { error } = validateUser(request.body);
-//     if (error) return response.status(400).send(error.details[0].message);
+export async function registerUser (request: Request, response: Response, next: NextFunction) {
+    const { error } = validateUser(request.body);
+    if (error) return response.status(400).send(error.details[0].message);
 
-//     let user = await User.findOne({ email: request.body.email})
-//     if (user) return response.status(400).send('User already registered');
+    let user = await User.findOne({ email: request.body.email})
+    if (user) return response.status(400).send('User already registered');
 
-//     user = new User({
-//         username: request.body.username,
-//         email: request.body.email,
-//         password: request.body.password
-//     });
+    user = new User({
+        username: request.body.username,
+        email: request.body.email,
+        password: request.body.password
+    });
 
-//     await user.save();
+    await user.save();
 
-//     response.send(user);
-// }
+    response.send(user);
+}
 
 export function getUsers(request: Request, response: Response) {
     response.send([]);
