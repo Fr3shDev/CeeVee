243a529dde4278275050fc8762c3b9ed8266d091
Fresh Dev <billyhans90@gmail.com>
2024-12-15 11:16:00 +0100

    Move registration to user handler file

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,25 +1,31 @@
 import { CreateUserDto } from "dtos/CreateUser.dto";
-import { NextFunction, Request, Response } from "express-serve-static-core";
+import {Request, Response } from "express-serve-static-core";
 import { User, validateUser } from "models/user";
 import { CreateUserQueryParams } from "types/query-params";
 import { UserResponseInterface } from "types/response";
+import _ from "lodash";
+import bcrypt from "bcrypt";
 
-export async function registerUser (request: Request, response: Response, next: NextFunction) {
+export async function registerUser (request: Request, response: Response) {
     const { error } = validateUser(request.body);
-    if (error) return response.status(400).send(error.details[0].message);
+    if (error) {
+        response.status(400).send(error.details[0].message);
+        return;
+    }
 
     let user = await User.findOne({ email: request.body.email})
-    if (user) return response.status(400).send('User already registered');
+    if (user) {
+        response.status(400).send('User already registered');
+        return;
+    }
 
-    user = new User({
-        username: request.body.username,
-        email: request.body.email,
-        password: request.body.password
-    });
+    user = new User(_.pick(request.body, ['username', 'email', 'password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
 
     await user.save();
 
-    response.send(user);
+    response.send(_.pick(user, ['id', 'email', 'username']));
 }
 
 export function getUsers(request: Request, response: Response) {
