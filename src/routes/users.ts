3b0a1d8a52eee905072ed15bdb7f594e660e6e5a
Fresh Dev <billyhans90@gmail.com>
2024-12-22 11:35:35 +0100

    Remove all unwanted methods

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,19 +1,10 @@
 import { Router } from "express";
-import { createUser, getUser, getUsers, registerUser } from "../controllers/auth";
+import { registerUser } from "../controllers/auth";
 
 const router = Router();
 
 
 
 router.post('/', registerUser);
-
-// /api/users
-router.get('/', getUsers);
-
-// /api/users/123
-router.get('/:id', getUser);
-
-// /api/users
-router.post('/', createUser)
 
 export default router;
