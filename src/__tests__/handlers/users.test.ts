d1e6b570d300f77b5b0365c12c3edb8912432042
Fresh Dev <billyhans90@gmail.com>
2024-12-22 11:33:05 +0100

    Update auth file name

--- src/__tests__/handlers/users.test.ts
+++ src/__tests__/handlers/users.test.ts
@@ -1,5 +1,5 @@
 import { mockRequest, mockResponse } from "../../__mocks__"
-import { getUsers } from "../../controllers/users"
+import { getUsers } from "../../controllers/auth"
 
 describe('getUsers', () => {
     it('should return an array of users', () => {
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createUser, getUser, getUsers, registerUser } from "../controllers/users";
+import { createUser, getUser, getUsers, registerUser } from "../controllers/auth";
 
 const router = Router();
 
