d9c731e9baec41d052aebffb13a7e15176a1d5c4
Fresh Dev <billyhans90@gmail.com>
2024-12-23 19:59:43 +0100

    Refactor the questionnaire model and service

--- src/services/questionnaire.ts
+++ src/services/questionnaire.ts
@@ -1,3 +1,25 @@
 import { QuestionnaireInterface } from "interfaces/questionnaire.interface";
 import Questionnaire from "../models/questionnaire";
 
+class QuestionnaireService {
+    async saveResponses(
+        userId: string,
+        responses: Record<string, string>,
+    ): Promise<QuestionnaireInterface> {
+        const questionnaireId = `qn-${Date.now()}`;     // Generate a unique ID for the questionnaire
+        const questionnaire = new Questionnaire({
+            userId,
+            questionnaireId,
+            responses
+        });
+
+        return await questionnaire.save();
+    }
+
+    // Retrieve user responses
+    async getResponses(userId: string): Promise<QuestionnaireInterface[]> {
+        return await Questionnaire.find({ userId});
+    }
+}
+
+export default new QuestionnaireService();
